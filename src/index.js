30313d953e919cd5941872b178e1b386b340ae39
hollywood2fresh <mael.boilevepro@gmail.com>
2023-05-13 15:21:59 +0200

    publish param

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/Projet6_Kasa'>
     <App />
   </BrowserRouter>
 );
